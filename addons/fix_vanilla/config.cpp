bfd532fd1e345ea0e44b7dda9b982e6548164d7d
johnb432 <58661205+johnb432@users.noreply.github.com>
2020-10-04 11:05:51 +0200

    Initial commit

---
+++ addons/fix_vanilla/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches {
+    class WB_fix_vanilla {
+        name = "Weapons balance - Fix basegame";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.96;
+        requiredAddons[] = {"ace_realisticnames"};
+        author = "johnb43";
+    };
+};
+
+#include "CfgAcc.hpp"
+#include "CfgMagazineWells.hpp"
+#include "CfgMagazines.hpp"
+#include "CfgWeapons.hpp"
+#include "CfgAmmo.hpp"
